241b5f2a957988082b516809ae2fcb659285a342
Shaik66-pro <Sameernawab66@gmail.com>
2026-06-13 21:10:03 +0530

    Add files via upload

---
+++ Day-17/lambda_functions.py
@@ -0,0 +1,460 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+for i in range(101,2)
+SyntaxError: expected ':'
+for i in range(101,2):
+    print(i)
+
+    
+
+
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+for i in range(101,2):
+    print(i)
+
+    
+
+
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+1
+1
+
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+51
+53
+55
+57
+59
+61
+63
+65
+67
+69
+71
+73
+75
+77
+79
+81
+83
+85
+87
+89
+91
+93
+95
+97
+99
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
+52
+54
+56
+58
+60
+62
+64
+66
+68
+70
+72
+74
+76
+78
+80
+82
+84
+86
+88
+90
+92
+94
+96
+98
+100
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100 
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+15
+30
+45
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+3 x 1 =
+3 x 2 =
+3 x 3 =
+3 x 4 =
+3 x 5 =
+3 x 6 =
+3 x 7 =
+3 x 8 =
+3 x 9 =
+3 x 10 =
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+3 x 1 = 1
+3 x 2 = 2
+3 x 3 = 3
+3 x 4 = 4
+3 x 5 = 5
+3 x 6 = 6
+3 x 7 = 7
+3 x 8 = 8
+3 x 9 = 9
+3 x 10 = 10
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+1
+1
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+
+s1
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    s1
+NameError: name 's1' is not defined
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+
+
+
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 14, in <module>
+    print(res(12))
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 13, in <lambda>
+    res = lambda ch: "Alp" if ch.isalpha() else "Not Alpha"
+AttributeError: 'int' object has no attribute 'isalpha'
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Not Alpha
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Alp
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Not Alpha
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 14, in <module>
+    print(res('2'))
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 13, in <lambda>
+    res = lambda num:num*num
+TypeError: can't multiply sequence by non-int of type 'str'
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+4
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+thank you 2 for choosing python
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+thank you Sameer for choosing python
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 19, in <module>
+    for i in range(l):
+TypeError: 'list' object cannot be interpreted as an integer
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 19, in <module>
+    for i in range(l):
+TypeError: 'list' object cannot be interpreted as an integer
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[1, 4, 9, 16, 25, 36]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[1, 2, 3, 4, 5, 6]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[1, 8, 27, 64, 125, 216]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[1, 8, 27, 64, 125, 216]
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 24, in <module>
+    res = list(map(lambda l:l**2))
+TypeError: map() must have at least two arguments.
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[1, 8, 27, 64, 125, 216]
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 24, in <module>
+    res = list(map(lambda i:l**2))
+TypeError: map() must have at least two arguments.
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[1, 8, 27, 64, 125, 216]
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 24, in <module>
+    res = list(map(lambda i:l**2))
+TypeError: map() must have at least two arguments.
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[1, 8, 27, 64, 125, 216]
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 24, in <module>
+    res = list(map(lambda l:l**2))
+TypeError: map() must have at least two arguments.
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 19, in <module>
+    res = list(map(lambda i:i.upper()))
+TypeError: map() must have at least two arguments.
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+['ASIF', 'PRIYA', 'YASHWANTH']
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 21, in <module>
+    res = list(map(lambda i:i+10,i))
+NameError: name 'i' is not defined. Did you mean: 'id'?
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[11, 12, 13, 14, 15]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[11, 12, 13, 14, 15]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 23, in <module>
+    res = list(map(lambda i:[0]*i[1],enumerate(1)))
+TypeError: 'int' object is not iterable
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 23, in <module>
+    res = list(map(lambda i:[0]*i[1],enumerate(1)))
+TypeError: 'int' object is not iterable
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 23, in <module>
+    res = list(map(lambda i:[0]*i[1],enumerate(1)))
+TypeError: 'int' object is not iterable
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 23, in <module>
+    res = list(map(lambda i:[0]*i[1],enumerate(1)))
+TypeError: 'int' object is not iterable
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 24, in <module>
+    print(res())
+TypeError: 'list' object is not callable
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 24, in <module>
+    print(res())
+TypeError: 'list' object is not callable
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 24, in <module>
+    print(res())
+TypeError: 'list' object is not callable
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+gmail.com
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+<function <lambda> at 0x000002AC1A16DD20>
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 30, in <module>
+    res =list(map(lambda i : i%3==0))
+TypeError: map() must have at least two arguments.
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 30, in <module>
+    res =list(filter(lambda i : i%3==0))
+TypeError: filter expected 2 arguments, got 1
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[3, 6, 9, 12]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[False, False, True, False, False, True, False, False, True, False, True]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[3, 6, 9, 12]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+['much', 'bank']
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[2, 4, 6, 98, 10, 12]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[False, True, False, True, False, True, False, True, False, True, True]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 40, in <module>
+    res = list(float(lambda i:i%2==0,l))
+TypeError: float expected at most 1 argument, got 2
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+[2, 4, 6, 98, 10, 12]
+
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 45, in <module>
+    res = reduce(lambda sum:i+sum,l)
+NameError: name 'reduce' is not defined
+>>> 
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 45, in <module>
+    res = reduce(lambda sum,i:i+sum,l)
+NameError: name 'reduce' is not defined
+>>> 
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 45, in <module>
+    res = reduce(lambda sum,i:sum+i,l)
+NameError: name 'reduce' is not defined
+>>> 
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Documents/1-05.py", line 45, in <module>
+    res = reduce(lambda sum,i:sum+i,l)
+NameError: name 'reduce' is not defined
+>>> 
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+157
+>>> 
+================================================================== RESTART: C:/Users/samee/OneDrive/Documents/1-05.py ==================================================================
+>>> 
+>>> sorted(d)
+['keerthi', 'priya', 'sameer']
+>>> d={'priya':60,'keerthi':95,'sameer':45}
+... sorted(d)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> sorted(d)
+['keerthi', 'priya', 'sameer']
+>>> dict(sorted(d.items()))
+{'keerthi': 95, 'priya': 60, 'sameer': 45}
+>>> dict(sorted(d.items(),reverse=false))
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    dict(sorted(d.items(),reverse=false))
+NameError: name 'false' is not defined. Did you mean: 'False'?
+>>> dict(sorted(d.items(),reverse=True))
+{'sameer': 45, 'priya': 60, 'keerthi': 95}
+>>> dict(sorted(d.items(),reverse=False))
+{'keerthi': 95, 'priya': 60, 'sameer': 45}
+>>> dict(sorted(d.items(),key=lambda i[1]))
+SyntaxError: invalid syntax
