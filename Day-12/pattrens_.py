75dbff2110f84a9e3a9eee13a8d0972c8f15c96e
Shaik66-pro <Sameernawab66@gmail.com>
2026-06-13 21:08:10 +0530

    Add files via upload

---
+++ Day-12/pattrens_.py
@@ -0,0 +1,41 @@
+'''
+n = int(input("Enter the number: "))
+for i in range(n):
+    for j in range(n):
+        if j == 0 or j == (n-1) or i == 0 or i == (n-1):
+            print('*',end =' ')
+        else:
+            print(' ',end =' ')
+    print()
+       '''
+'''Z
+n = int(input("Enter the number: "))
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == (n-1) or i+j==(n-1):
+            print('*',end =' ')
+        else:
+            print(' ',end =' ')
+    print()
+'''
+
+'''X
+n = int(input("Enter the number: "))
+for i in range(n):
+    for j in range(n):
+        if i == j or i+j==(n-1):
+            print('*',end =' ')
+        else:
+            print(' ',end =' ')
+    print()
+'''
+
+n = int(input("Enter the number: "))
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == (n-1) or j ==0 or i == (n-1):
+            print('*',end =' ')
+        else:
+            print(' ',end =' ')
+    print()
+
