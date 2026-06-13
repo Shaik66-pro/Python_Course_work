46dabb768e8c3ec82b3bb85fdbfa27fdb79e4075
Shaik66-pro <Sameernawab66@gmail.com>
2026-06-13 21:07:21 +0530

    Add files via upload

---
+++ Day-9/12.py
@@ -0,0 +1,55 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+{1: {'name': 'pen', 'tag': 'best seller'}}
+enter best seller:
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+{1: {'name': 'pen', 'tag': 'best seller'}}
+enter products:
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+{1: {'name': 'pen', 'tag': 'best seller'}, 2: {'name': 'pencil', 'discount': 10}}
+enter products:pen
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py", line 6, in <module>
+    index=int(input("enter products:"))
+ValueError: invalid literal for int() with base 10: 'pen'
+>>> 'pen'
+'pen'
+>>> 
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+Enter the skills:python
+sorry,try again. you need this skills set: {'c#', 'html', 'javascript', 'css'}
+>>> 
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+{1: {'name': 'pen', 'tag': 'best seller'}, 2: {'name': 'pencil', 'discount': 10}}
+enter products:
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+Enter the skills:python javascript
+sorry,try again. you need this skills set: {'css', 'c#', 'html'}
+>>> 
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+Enter the skills:
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+Enter the Persons:1
+Traceback (most recent call last):
+  File "C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py", line 35, in <module>
+    if persons == 1:
+NameError: name 'persons' is not defined. Did you mean: 'person'?
+>>> 
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+Enter the Persons:3
+Your trolly is on the way. Please pay $5
+>>> 
+>>> 
+>>> 
+>>> 2
+2
+>>> 
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+Enter the Persons:2
+Your riskshaw is on the way. Please pay $15
+>>> 
+================================================ RESTART: C:/Users/samee/OneDrive/Desktop/Python_Course_Work/Da-9/conditionalstaments.py ===============================================
+Enter the Persons:1
+Your bike is on the way. Please Pay $10
---
+++ Day-9/conditionalstaments.py
@@ -0,0 +1,47 @@
+'''
+
+
+
+
+products= {
+    1:{'name':'pen','tag':'best seller'},
+    2:{'name':'pencil','discount':10}        
+           }
+print(products)
+index=int(input("enter products:"))
+if products [index]['best seller']:
+          print(f'{products[index]['name']} best discount')        
+
+
+'''
+
+'''
+
+#resume short listed
+jd = {'python','html','css','javascript','c#'}
+skills = set(input("Enter the skills:").split())
+if jd == skills:
+    print("congrates!! your resume is shortlisted")
+else:
+    print(f"sorry,try again. you need this skills set: {jd-skills}")
+
+
+'''    
+
+
+
+
+persons = int(input("Enter the Persons:"))
+if persons == 1:
+    print("Your bike is on the way. Please Pay $10")
+elif persons == 2:
+    print("Your riskshaw is on the way. Please pay $15")
+elif persons == 3:
+        print("Your trolly is on the way. Please pay $5")
+elif persons == 4:
+        print("Your car is on the way. Please pay $50")
+else:
+        print("Enter the valid input:")
+    
+    
+          
